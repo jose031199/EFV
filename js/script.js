76dd3cf66d41e693f5bd3da49c17b51e850d1127
jose031199 <luisking531@gmail.com>
2021-11-11 16:42:06 -0800

    Creando Archivo script.js y una funcion para cerrar menu mobil

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded',function() {
+    eventListener();
+});
+
+function eventListener() {
+  const barra = document.querySelector('.bars');
+  const cerrar = document.querySelector('.close');
+  const nav_links = document.querySelector('.nav-links');
+
+  barra.addEventListener('click',() => {
+    nav_links.classList.add('mostrar');
+  });
+
+  cerrar.addEventListener('click',() => {
+    nav_links.classList.remove('mostrar');
+  });
+
+
+}
