72dcc34dd93a54af85684ba1a5a2e805ede5e371
jose031199 <luisking531@gmail.com>
2021-11-21 19:47:56 -0800

    Creando funcion alerta para mostrar errores al llenar el formulario

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
+const persona ={nombre:''};
 document.addEventListener('DOMContentLoaded',function() {
     eventListener();
+   
+    //Guardar nombre del usuario
+    persona_nombre();
 });
 
 function eventListener() {
@@ -14,6 +18,38 @@ function eventListener() {
   cerrar.addEventListener('click',() => {
     nav_links.classList.remove('mostrar');
   });
+}
 
+function persona_nombre(){
+  const input_Nombre = document.querySelector('#name');
+
+  input_Nombre.addEventListener('input',(e)=>{
+   const nombreTexto = e.target.value.trim();
+  
+   if(nombreTexto.length<4 || nombreTexto===''){
+     //Funcion para mostrar mensaje
+    MostrarAlerta("Nombre no valido")
+     }else{
+      persona.nombre = nombreTexto;
+     }
+  });
+}
+
+function MostrarAlerta(message){
+  const alertaPrevia = document.querySelector('.alerta');
+
+  //Si existe alerta
+  if(alertaPrevia){
+    return;
+  }
+
+  //Crear alerta
+  const alerta = document.createElement('DIV');
+  alerta.textContent = message;
+  alerta.classList.add('alerta');
+
+  //Agregarlo en el formulario
+  const formulario = document.querySelector('#formulario');
+  formulario.appendChild(alerta);
 
 }
