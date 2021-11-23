4b36fab2e9865895ff9e2eccfbffd074a4afb49b
jose031199 <luisking531@gmail.com>
2021-11-22 21:05:06 -0800

    Agregando funcion para guardar email,subject y mensaje

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const persona ={nombre:'',
-  email: ''};
+email:'',subject:'',message:'',};
 document.addEventListener('DOMContentLoaded',function() {
     eventListener();
    
@@ -35,7 +35,7 @@ function eventListener() {
   });
 }
 
-function persona_datos(){
+function persona_nombre(){
   const input_Nombre = document.querySelector('#name');
   //const input_email = document.querySelector('#email');
   //const input_subject = document.querySelector('#subject');
@@ -56,6 +56,59 @@ function persona_datos(){
   });
 }
 
+function persona_email(){
+  const input_email = document.querySelector('#email');
+
+  input_email.addEventListener('input',(e)=>{
+    const email_regex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
+    const emailTexto = e.target.value.trim();
+    if(emailTexto===''||!emailTexto.match(email_regex)){
+      MostrarAlerta('Correo no Válido');
+    }else{
+      const alerta = document.querySelector('.alerta');
+    if(alerta){
+      alerta.remove();
+    }
+    persona.email = emailTexto;
+    }
+    
+  });
+}
+
+function persona_subject(){
+  const input_subject = document.querySelector('#subject');
+
+  input_subject.addEventListener('input',(e)=>{
+    const inputTexto = e.target.value.trim();
+    if(inputTexto===''||inputTexto.length<5){
+      MostrarAlerta('Subject es muy corto')
+    }else{
+      const alerta = document.querySelector('.alerta');
+      if(alerta){
+        alerta.remove();
+      }
+      persona.subject = inputTexto;
+    }
+  });
+}
+
+function persona_message(){
+  const input_message = document.querySelector('#message');
+
+  input_message.addEventListener('input',(e)=>{
+    const messageTexto = e.target.value.trim();
+    if(input_message===''||input_message.length<15){
+      MostrarAlerta('Agrega más contenido');
+    }else{
+      const alerta = document.querySelector('.alerta');
+      if(alerta){
+        alerta.remove();
+      }
+      persona.message = messageTexto;
+    }
+  });
+}
+
 
 
 
@@ -69,7 +122,10 @@ function MostrarAlerta(message){
 
   //Crear alerta
   const alerta = document.createElement('DIV');
-  alerta.textContent = message;
+  const mensaje = document.createElement('p');
+  mensaje.textContent= message;
+  alerta.appendChild(mensaje);
+  //alerta.textContent = message;
   alerta.classList.add('alerta');
 
   //Agregarlo en el formulario
@@ -80,5 +136,5 @@ function MostrarAlerta(message){
 
   setTimeout(() => {
     alerta.remove();
-  },5000);
+  },8000);
 }
